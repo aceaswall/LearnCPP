cfce4942df030696edc49fb879f7cb866cef303f
aceaswall <aceaswall@gmail.com>
2024-06-19 16:01:50 -0300

    Add

---
+++ LearnCPP/ReadingDocumentation.cpp
@@ -0,0 +1,23 @@
+// Copyright A.T. Chamillard. All Rights Reserved.
+
+#define _USE_MATH_DEFINES
+
+#include <iostream>
+
+/**
+ * Demonstrates reading documentation
+ * @return Exit status
+*/
+
+int main()
+{
+    float Angle{ 45 };
+
+    // convert angle to radians
+    Angle = Angle * M_PI / 180;
+
+
+    // calculate and print the cosine of 45 deegress 
+    float Cosine = cosf(Angle);
+    std::cout << "Cosine of 45 degress: " << Cosine << "\n";
+}
