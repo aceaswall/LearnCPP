aec7eceb8afa75af01177ae44963b2eb34bfd01c
aceaswall <aceaswall@gmail.com>
2024-06-21 14:20:58 -0300

    Updates and add new files

--- LearnCPP/FirstConsoleApp.cpp
+++ LearnCPP/FirstConsoleApp.cpp
@@ -1,13 +1,13 @@
 // Copyright A.T. Chamillard. All Rights Reserved.
 
-#include <iostream>
+//#include <iostream>
 
 /**
  * Prints something
  * @return Exit status
 */
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+//int main()
+//{
+//    std::cout << "Hello World!\n";
+//}
